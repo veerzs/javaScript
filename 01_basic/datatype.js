379c2056897d82f07577a4c7f839fbe652b9641c
veerzs <125331074+veerzs@users.noreply.github.com>
2024-03-30 16:28:08 +0000

    dtatatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,15 @@
+// permitive datatype
+// String
+// Number
+// Boolean
+// null
+// BigInt
+// float
+// double
+//it is store in stack
+//------------------------------------------//
+//non permitive
+// Array
+// obeject
+// function
+//it is store in heap 
---
+++ 01_basic/variables.js
@@ -0,0 +1,5 @@
+ const accountId=123456
+ let accountEmail='veerrao@1323@gmail.com'
+ let password='v12345678'
+ var city='delhi'
+ console.table([accountEmail,accountId,password,city])
